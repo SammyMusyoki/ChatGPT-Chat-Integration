db1e0445db235df7ea54981cb442b7e9107f4209
SammyMusyoki <75688249+SammyMusyoki@users.noreply.github.com>
2023-07-30 22:07:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChatGPT-Chat-Integration
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-yykbnq)
---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import './style.css';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from '@chatscope/chat-ui-kit-react';
+
+const API_KEY = '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';
+
+export default function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm SammyGPT-3.5! Ask me anything",
+      sentTime: 'just now',
+      sender: 'ChatGPT',
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: 'user',
+    };
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+
+    // Initial system message to determine ChatGPT functionality
+    // How it talks, how it responds etc.
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = '';
+      if (messageObject.sender === 'ChatGPT') {
+        role = 'assistant';
+      } else {
+        role = 'user';
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      model: 'gpt-3.5-turbo',
+      messages: [systemMessage, ...apiMessages],
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    };
+
+    await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + API_KEY,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: 'ChatGPT',
+          },
+        ]);
+        setIsTyping(false);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <div style={{ position: 'relative', height: '100vh', width: '100%' }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                isTyping ? (
+                  <TypingIndicator content="ChatGPT is typing..." />
+                ) : null
+              }
+            >
+              {messages.map((message, index) => {
+                return <Message key={index} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput placeholder="Message" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </React.Fragment>
+  );
+}
